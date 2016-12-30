b94822173f7fd13327b82316ef2711119c298702
CattleRoyale <mslagter23@gmail.com>
2016-12-30 17:13:54 +0000

    added tests

--- src/main/java/com/JavaStudyGroup/Projects/ClockCreater.java
+++ src/main/java/com/JavaStudyGroup/Projects/ClockCreater.java
@@ -9,6 +9,14 @@ public class ClockCreater {
     private int minutes;
     private int seconds;
 
+    private char displaySeconds;
+
+    private char[] displayHoursRow1 = new char[4];
+    private char[] displayHoursRow2 = new char[4];
+
+    private char[] displayMinutesRow1 = new char[11];
+    private char[] displayMinutesRow2 = new char[4];
+
     public ClockCreater (String time){
         System.out.println("Parsing hours...");
         this.hours = Integer.parseInt(time.substring(0,2));
@@ -19,6 +27,46 @@ public class ClockCreater {
         System.out.println("Parsting seconds...");
         this.seconds = Integer.parseInt(time.substring(7,9));
         System.out.println("Seconds: " + seconds);
+    }
+
+    public void createSeconds(){
+        if(seconds%2==0){
+            displaySeconds = 'Y';
+        }
+        else{
+            displaySeconds = 'O';
+        }
+    }
+
+    public void createHours(){
+
+        int row1BoxesToFill = hours/5;
+        int row2BoxesToFill = hours%5;
+
+
+        //Create Line 1:
+        for(int i = 0;i<4;i++){
+            if (i<row1BoxesToFill){
+                displayHoursRow1[i] = 'R';
+            }
+            else{
+                displayHoursRow1[i] = 'O';
+            }
+        }
+
+        //Create Line 2
+        for(int i = 0;i<4;i++){
+            if (i<row2BoxesToFill){
+                displayHoursRow2[i] = 'R';
+            }
+            else{
+                displayHoursRow2[i] = 'O';
+            }
+        }
+    }
+
+    public void createMinutes(){
+
     }
 
     public int getHours() {
@@ -43,5 +91,21 @@ public class ClockCreater {
 
     public void setSeconds(int seconds) {
         this.seconds = seconds;
+    }
+
+    public char getDisplaySeconds() {
+        return displaySeconds;
+    }
+
+    public void setDisplaySeconds(char displaySeconds) {
+        this.displaySeconds = displaySeconds;
+    }
+
+    public char[] getDisplayHoursRow1() {
+        return displayHoursRow1;
+    }
+
+    public char[] getDisplayHoursRow2() {
+        return displayHoursRow2;
     }
 }
--- src/test/java/com/JavaStudyGroup/Projects/ClockCreaterTest.java
+++ src/test/java/com/JavaStudyGroup/Projects/ClockCreaterTest.java
@@ -10,6 +10,38 @@ import static org.junit.Assert.*;
  */
 public class ClockCreaterTest {
     @Test
+    public void createSecondsEven() throws Exception {
+        ClockCreater clock = new ClockCreater("13::09:52");
+        clock.createSeconds();
+        assertThat(clock.getDisplaySeconds(), is ('Y'));
+    }
+
+    @Test
+    public void createSecondsOdd() throws Exception {
+        ClockCreater clock = new ClockCreater("13::09:53");
+        clock.createSeconds();
+        assertThat(clock.getDisplaySeconds(), is ('O'));
+    }
+
+    @Test
+    public void createHours() throws Exception {
+        ClockCreater clock = new ClockCreater("13::09:53");
+        clock.createHours();
+        char[] hoursRow1 = clock.getDisplayHoursRow1();
+        assertThat(hoursRow1[0], is('R'));
+        assertThat(hoursRow1[1], is('R'));
+        assertThat(hoursRow1[2], is('O'));
+        assertThat(hoursRow1[3], is('O'));
+
+    }
+
+    @Test
+    public void createMinutes() throws Exception {
+
+    }
+
+
+    @Test
     public void getHours() throws Exception {
         ClockCreater clock = new ClockCreater("13::09:53");
         assertThat(clock.getHours(), is (13));
